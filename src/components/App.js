1b6e288efa9f6bece05cf8b0027cac9076b53496
VishalSaini7272 <sainivishal156@gmail.com>
2025-10-29 20:05:01 +0530

    Refactor App.js: Improve button and key handling

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import '../styles/App.css';
 
 class App extends Component {
@@ -14,19 +14,38 @@ class App extends Component {
     };
 
     buttonClickHandler() {
-   
+		this.setState({renderBall : true})
    }
+
+	//handle arrow right key press
+	handleDownKet(event){
+		if(event.keyCode === 39){
+			//move 5px right
+			this.setState((prevState) => {
+				const newPos = prevState.posi + 5;
+				return {
+					posi : newPos,
+					 ballPosition: { left: newPos + "px" }
+				}
+			}
+	   }
+	}
+
+
+
+
+	
     renderBallOrButton() {
 		if (this.state.renderBall) {
 		    return <div className="ball" style={this.state.ballPosition}></div>
 		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
+		    return <button className="start" onClick={this.buttonClickHandler} >Start</button>
 		}
     }
 
     // bind ArrowRight keydown event
     componentDidMount() {
-      
+      document.addEventListener("keyDown",this.handleKeyDown)
     }
 
     render() {
